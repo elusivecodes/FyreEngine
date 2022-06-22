19c491dadbd6c1714763d0bf331e89a775edb632
Elusive <elusivecodes@gmail.com>
2022-06-22 18:40:21 +1000

    - Added BehaviorRegistry handling to bootstrap method. - Added ComponentRegistry handling to bootstrap method. - Added HelperRegistry handling to bootstrap method. - Updated composer.

--- src/Engine.php
+++ src/Engine.php
@@ -6,14 +6,17 @@ namespace Fyre\Engine;
 use
     Fyre\Command\CommandRunner,
     Fyre\Config\Config,
+    Fyre\Controller\ComponentRegistry,
     Fyre\Lang\Lang,
     Fyre\Middleware\MiddlewareQueue,
     Fyre\Middleware\RequestHandler,
     Fyre\Migration\MigrationRunner,
+    Fyre\ORM\BehaviorRegistry,
     Fyre\ORM\EntityLocator,
     Fyre\ORM\ModelRegistry,
     Fyre\Router\Router,
     Fyre\Server\ServerRequest,
+    Fyre\View\HelperRegistry,
     Fyre\View\View;
 
 use const
@@ -36,11 +39,13 @@ abstract class Engine
         Lang::addPath(LANG);
         View::addPath(TEMPLATES);
 
+        BehaviorRegistry::addNamespace('App\Model\Behaviors');
         CommandRunner::addNamespace('App\Command');
+        ComponentRegistry::addNamespace('App\Controller\Components');
         EntityLocator::addNamespace('App\Entity');
         MigrationRunner::setNamespace('App\Migration');
         ModelRegistry::addNamespace('App\Model');
-        View::addNamespace('App\View\Helpers');
+        HelperRegistry::addNamespace('App\View\Helpers');
 
         Config::load('functions');
         Config::load('app');
